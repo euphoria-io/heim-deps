88407a12822ce31c7539fa6e1a07c012cf021d07
Max Goodman <c@chromakode.com>
2015-02-16 12:33:16 -0800

    Add deps helper script.

---
+++ deps.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+abspath() { cd $(dirname $1); echo $(pwd)/$(basename $1); }
+
+usage="USAGE: $0 (sync|update) <heim-dir>"
+SRCDIR=$(dirname `abspath $0`)
+
+sync_deps() {
+  rsync -rut --delete $SRCDIR/node_modules $1/client
+}
+
+update_deps() {
+  cd $SRCDIR
+  cp $1/client/package.json ./
+  npm install
+  rm package.json
+  git status
+}
+
+if [[ "$1" = "" || "$2" = "" ]]; then
+  echo $usage
+  exit 1
+fi
+
+case $1 in
+  sync)
+    sync_deps $2
+    ;;
+  update)
+    update_deps $2
+    ;;
+  *)
+    echo $usage
+    exit 1
+esac
