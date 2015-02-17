949afc3a60e71a3c97ee5708ae61f431f58f4100
Max Goodman <c@chromakode.com>
2015-02-17 13:48:02 -0800

    Link deps instead of copying.

--- deps.sh
+++ deps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 abspath() { cd $(dirname $1); echo $(pwd)/$(basename $1); }
 
-usage="USAGE: $0 (sync|update) <heim-dir>"
+usage="USAGE: $0 (link|update) <heim-dir>"
 
-sync_deps() {
-  rsync -rlt --delete $SRCDIR/node_modules $HEIMDIR/client
+link_deps() {
+  mkdir -p $HEIMDIR/deps
+  ln -s $SRCDIR/node_modules $HEIMDIR/deps/node_modules
+  ln -s $SRCDIR/godeps $HEIMDIR/deps/godeps
 }
 
 update_js_deps() {
@@ -56,8 +58,8 @@ cd $SRCDIR
 set -x
 
 case $1 in
-  sync)
-    sync_deps
+  link)
+    link_deps
     ;;
   update-go)
     update_go_deps
