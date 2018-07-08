af5d6fdca8983a96592290919b673bbd20f0b814
Logan Hanks <logan.hanks@gmail.com>
2018-07-07 17:50:41 -0700

    Remove go support from deps.sh

--- deps.sh
+++ deps.sh
@@ -32,32 +32,6 @@ print_js_versions() {
   echo "node `node -v`; npm `npm -v`"
 }
 
-update_go_deps() {
-  rm -rf godeps
-  mkdir -p godeps/src/euphoria.io
-  cp -r $HEIMDIR/ godeps/src/euphoria.io/heim
-
-  GOPATH=`pwd`/godeps go get -d -t -x ./godeps/src/euphoria.io/heim/... github.com/coreos/etcd
-
-  # pin go-etcd to v0.4.6 (until coreos upgrades to etcd 2.0)
-  git --git-dir=godeps/src/github.com/coreos/go-etcd/.git \
-      --work-tree=godeps/src/github.com/coreos/go-etcd \
-      checkout 6aa2da5
-
-  # save the commit hash of deps but remove the git metadata so we don't create submodules.
-  find godeps -depth -name .git -type d -execdir sh -c "git rev-parse HEAD > git-HEAD && rm -rf .git" \;
-
-  # and likewise for hg repos
-  find godeps -depth -name .hg -type d -execdir sh -c "hg parent -T '{node}\n' > hg-HEAD && rm -rf .hg" \;
-
-  rm -rf godeps/src/euphoria.io/heim
-}
-
-print_go_versions() {
-  set +x
-  go version
-}
-
 if [[ "$1" = "" || "$2" = "" ]]; then
   echo $usage
   exit 1
@@ -70,11 +44,6 @@ cd $SRCDIR
 set -x
 
 case $1 in
-  update-go)
-    update_go_deps
-    print_go_versions
-    date
-    ;;
   update-js)
     update_js_deps
     print_js_versions
@@ -86,9 +55,7 @@ case $1 in
     date
     ;;
   update)
-    update_go_deps
     update_js_deps
-    print_go_versions
     print_js_versions
     date
     ;;
